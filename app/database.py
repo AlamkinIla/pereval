1be5706029ffe8c02a3da73bee263efe3ba9b2c2
Licht Bach <i_sumsung@mail.ru>
2026-01-14 22:49:06 +0300

    Add GET and PATCH

--- app/database.py
+++ app/database.py
@@ -30,3 +30,89 @@ class Database:
             pereval_id = cursor.fetchone()[0]
             self.connection.commit()
             return pereval_id
+
+    def get_pereval_by_id(self, pereval_id: int):
+        with self.conn.cursor() as cursor:
+            cursor.execute(
+                """
+                SELECT id, date_added, raw_data, images, status
+                FROM pereval_added
+                WHERE id = %s
+                """,
+                (pereval_id,)
+            )
+            result = cursor.fetchone()
+
+        if result is None:
+            return None
+
+        return {
+            "id": result[0],
+            "date_added": result[1],
+            "raw_data": result[2],
+            "images": result[3],
+            "status": result[4]
+        }
+
+    def get_perevals_by_email(self, email: str):
+        with self.conn.cursor() as cursor:
+            cursor.execute(
+                """
+                SELECT id, date_added, raw_data, images, status
+                FROM pereval_added
+                WHERE raw_data - > 'user' ->>'email' = %s
+                """,
+                (email,)
+            )
+            rows = cursor.fetchall()
+
+        result = []
+        for row in rows:
+            result.append({
+                "id": row[0],
+                "date_added": row[1],
+                "raw_data": row[2],
+                "images": row[3],
+                "status": row[4]
+            })
+
+        return result
+
+    def update_pereval(self, pereval_id: int, new_raw_data: dict, new_images: dict):
+        with self.conn.cursor() as cursor:
+            cursor.execute(
+                "SELECT raw_data, status FROM pereval_added WHERE id = %s",
+                (pereval_id,)
+            )
+            row = cursor.fetchone()
+
+            if row is None:
+                return 0, "Запись не найдена"
+
+            old_raw_data, status = row
+
+            if status != "new":
+                return 0, "Редактирование запрещено: статус не 'new'"
+
+            # запрещённые поля
+            user_fields = ["email", "fam", "name", "otc", "phone"]
+            for field in user_fields:
+                if (
+                        new_raw_data.get("user", {}).get(field)
+                        != old_raw_data.get("user", {}).get(field)
+                ):
+                    return 0, f"Поле user.{field} изменять нельзя"
+
+            cursor.execute(
+                """
+                UPDATE pereval_added
+                SET raw_data = %s,
+                    images   = %s
+                WHERE id = %s
+                """,
+                (Json(new_raw_data), Json(new_images), pereval_id)
+            )
+
+            self.conn.commit()
+
+        return 1, "Запись успешно обновлена"
--- app/main.py
+++ app/main.py
@@ -19,3 +19,27 @@ def submit_data(data: SubmitData):
         "message": "Отправлено успешно",
         "id": pereval_id
     }
+
+@app.get("/submitData/{pereval_id}")
+def get_submit_data(pereval_id: int):
+    result = db.get_pereval_by_id(pereval_id)
+    if result is None:
+        return {"message": "Запись не найдена"}
+    return result
+
+@app.get("/submitData/")
+def get_submit_data_by_email(user__email: str):
+    return db.get_perevals_by_email(user__email)
+
+@app.patch("/submitData/{pereval_id}")
+def update_submit_data(pereval_id: int, data: SubmitData):
+    state, message = db.update_pereval(
+        pereval_id=pereval_id,
+        new_raw_data=data.raw_data,
+        new_images=data.images
+    )
+
+    return {
+        "state": state,
+        "message": message
+    }
